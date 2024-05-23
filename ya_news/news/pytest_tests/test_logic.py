8b7f686feefc0dbe0dd76a7bff7eeff1414acf07
Andrey Mirt <nrmously@gmail.com>
2024-05-23 20:05:23 +0300

    Second commit

--- ya_news/news/pytest_tests/test_logic.py
+++ ya_news/news/pytest_tests/test_logic.py
@@ -5,8 +5,11 @@ from news.models import Comment
 
 
 @pytest.mark.django_db
-def test_anonymous_user_cant_create_comment(client, setup_comment_creation_data):
-    client.post(setup_comment_creation_data['url'], data=setup_comment_creation_data['form_data'])
+def test_anonym_user_cant_create_comment(client, setup_comment_creation_data):
+    client.post(
+        setup_comment_creation_data['url'],
+        data=setup_comment_creation_data['form_data']
+    )
     comments_count = Comment.objects.count()
     assert comments_count == 0
 
@@ -52,7 +55,7 @@ def test_author_can_delete_comment(setup_comment_edit_delete_data):
 
 
 @pytest.mark.django_db
-def test_user_cant_delete_comment_of_another_user(setup_comment_edit_delete_data):
+def test_user_cant_delete_foreign_сomment(setup_comment_edit_delete_data):
     response = setup_comment_edit_delete_data['reader_client'].delete(
         setup_comment_edit_delete_data['delete_url']
     )
@@ -70,15 +73,17 @@ def test_author_can_edit_comment(setup_comment_edit_delete_data):
     assert response.status_code == HTTPStatus.FOUND
     assert response.url == setup_comment_edit_delete_data['url_to_comments']
     setup_comment_edit_delete_data['comment'].refresh_from_db()
-    assert setup_comment_edit_delete_data['comment'].text == setup_comment_edit_delete_data['form_data']['text']
+    assert setup_comment_edit_delete_data['comment'].text == \
+        setup_comment_edit_delete_data['form_data']['text']
 
 
 @pytest.mark.django_db
-def test_user_cant_edit_comment_of_another_user(setup_comment_edit_delete_data):
+def test_user_cant_edit_foreign_сomment(setup_comment_edit_delete_data):
     response = setup_comment_edit_delete_data['reader_client'].post(
         setup_comment_edit_delete_data['edit_url'],
         data=setup_comment_edit_delete_data['form_data']
     )
     assert response.status_code == HTTPStatus.NOT_FOUND
     setup_comment_edit_delete_data['comment'].refresh_from_db()
-    assert setup_comment_edit_delete_data['comment'].text == 'Текст комментария'
+    assert setup_comment_edit_delete_data['comment'].text == \
+        'Текст комментария'
