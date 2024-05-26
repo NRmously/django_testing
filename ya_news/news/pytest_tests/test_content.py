e8d99e125fbb7df5e4b057cb83ebd1c0f621af66
Andrey Mirt <nrmously@gmail.com>
2024-05-26 23:54:11 +0300

    Fix test_order_comments.

--- ya_news/news/pytest_tests/test_content.py
+++ ya_news/news/pytest_tests/test_content.py
@@ -31,7 +31,7 @@ def test_order_comments(client, setup_detail_page_data):
     news = response.context['news']
     all_comments = news.comment_set.all()
     all_timestamps = [comment.created for comment in all_comments]
-    sorted_timestamps = sorted(all_timestamps, reverse=True)
+    sorted_timestamps = sorted(all_timestamps)
     assert all_timestamps == sorted_timestamps
 
 
